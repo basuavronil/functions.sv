b89873ed1909472a2444bfb685022ef9a8f13db2
Avronil <basuavronil65@gmail.com>
2026-03-18 12:14:18 +0530

    Update func_without_arg.sv

--- func_without_arg.sv
+++ func_without_arg.sv
@@ -1,14 +1,17 @@
 module test12;
   int b,c ;
-  function int sample (); //passing no input arguements 
+  function int sample ();//passing no input arguements 
     int temp = 0;
     sample = temp + 1;
     temp = sample;
   endfunction
   initial begin 
-    sample ();
-    sample ();
-    sample ();
+    b = sample ();
+    $display ("b = %0d temp = %0d ", b, sample.temp);  // accessing static variables local to a function using function_name.Local_variable
+    b = sample ();
+    $display ("b = %0d  temp = %0d", b, sample.temp);
+    b = sample ();
+    $display ("b = %0d temp = %0d", b, sample.temp);
+    
   end
 endmodule
-//outputs : 1  2  3 
